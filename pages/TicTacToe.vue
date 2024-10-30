740ffbd15d686a1bf0f483b3d6e1c6474e0f2ab3
Kreibach <kreibach@ksu.edu>
2024-10-30 18:31:57 -0500

    commit

--- pages/TicTacToe.vue
+++ pages/TicTacToe.vue
@@ -5,23 +5,13 @@
     
     <PrimePanel header="Does this work?" class="my-6">
       <h1>Play Tic Tac Toe</h1>
-      <div id="turn">x's Turn</div>
+      <div id="turn">{{ currentTurn }}'s Turn</div>
       <table>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
+          <tr v-for="(row, rowIndex) in board" :key="rowIndex">
+            <td v-for="(cell, colIndex) in row" :key="colIndex" @click="makeMove(rowIndex, colIndex)">
+              {{ cell }}
+            </td>
           </tr>
         </tbody>
       </table>
@@ -29,18 +19,46 @@
   </template>
   
   <script>
-  import script from '@/assets/script.js'; // Adjust based on your exports
-  
   export default {
-    mounted() {
-      if (typeof script.init === 'function') {
-        script.init(); // Call init if it exists
+    data() {
+      return {
+        board: [
+          ['', '', ''],
+          ['', '', ''],
+          ['', '', '']
+        ],
+        currentTurn: 'X',
+      };
+    },
+    methods: {
+      makeMove(row, col) {
+        if (this.board[row][col] === '') {
+          this.$set(this.board[row], col, this.currentTurn);
+          this.currentTurn = this.currentTurn === 'X' ? 'O' : 'X'; // Switch turns
+        }
       }
     }
   };
   </script>
   
   <style scoped>
-  /* Add your CSS here if needed */
+  /* Add your CSS here for styling the table and cells */
+  table {
+    border-collapse: collapse;
+    margin: 20px 0;
+  }
+  
+  td {
+    width: 50px;
+    height: 50px;
+    text-align: center;
+    border: 1px solid #000;
+    cursor: pointer;
+    font-size: 24px;
+  }
+  
+  td:hover {
+    background-color: #f0f0f0;
+  }
   </style>
   
