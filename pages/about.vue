1b54099e796c427ed027169f64963d633332db68
Kreibach <kreibach@ksu.edu>
2024-10-30 17:21:04 -0500

    commit

--- pages/about.vue
+++ pages/about.vue
@@ -1,28 +1,28 @@
 <template>
-	<link href="fancy.css" type="text/css" rel="stylesheet"/>
 	<div id="app">
-	  <img :src="image" alt="Main Image"/>
+	  <img :src="image" alt="Main Image" />
 	  
 	  <h1>Does this work?</h1>
-  
-	  <!-- Moved <head> content to <style> and <script> sections outside <template> -->
 	  
 	  <PrimePanel header="About Me!" class="my-6">
 		About me
 		<PrimeButton label="Button!" icon="pi pi-check" />
 	  </PrimePanel>
-  
+	
 	  <PrimePanel header="Personal Artwork" class="my-6">
-		<img :src="require('@/static/fly.jpg')" alt="Fly Image">
+		<img :src="flyImage" alt="Fly Image" />
 	  </PrimePanel>
 	</div>
   </template>
   
   <script>
+  import flyImage from '@/assets/fly.jpg';
+  
   export default {
 	data() {
 	  return {
-		image: require('@/assets/fly.jpg')
+		image: flyImage,
+		flyImage: flyImage
 	  };
 	}
   };
