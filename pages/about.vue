4c958d65838c921920423a7f7e5c11ea6d0c7be6
Kreibach <kreibach@ksu.edu>
2024-10-30 16:49:12 -0500

    image

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,33 @@
 <template>
+	<div id="app">
+        <img :src="image"/>
+    </div>
+
 	<h1>Does this work?</h1>
 	<head>
 		<link href="fancy.css" type="text/css" rel="stylesheet"/>
 	</head>
+
 	<PrimePanel header="About Me!" class="my-6">
 		About me
 		<PrimeButton label="Button!" icon="pi pi-check" />
 	</PrimePanel>
+
 	<PrimePanel header="Personal Atwork" class="my-6">
-		<img src=".fly.jpg">
+		<img src="flyImage" alt="Fly Image">
 	</PrimePanel>
 </template>
+
+<script>
+export default
+{
+	data()
+	{
+		return 
+		{
+			image: require('@/assets/fly.jpg')
+			flyImage: require*'@/assets/fly.jpg'
+		};
+	}
+};
+</script>
