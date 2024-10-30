fc645b271df8f36dc66d2bb6c528d571cc860764
Kreibach <kreibach@ksu.edu>
2024-10-30 18:22:28 -0500

    commit

--- pages/TicTacToe.vue
+++ pages/TicTacToe.vue
@@ -1,55 +1,46 @@
 <template>
-    <title>Tic Tac Toe</title>
-    <link href="fancy.css" type="text/css" rel="stylesheet"/>
+    <PrimePanel header="Hello!" class="my-6">
+      Example new page
+    </PrimePanel>
     
-	<PrimePanel header="Hello!" class="my-6">
-		Example new page
-	</PrimePanel>
     <PrimePanel header="Does this work?" class="my-6">
-		<head>
-		<title>Tic Tac Toe</title>
-		
-	    </head>
-
-        <body>
-            <h1>Play Tic Tac Toe</h1>
-            <div id="turn">x's Turn</div>
-            <table>
-                <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
-
-            
-        </body>
-	</PrimePanel>
-    
-</template>
-
-<script>
-// Import your JavaScript file
-import script from '@/assets/script.js';
-
-export default {
-  mounted() {
-    // Call a function from your script.js if it exports one
-    if (typeof script.init === 'function') {
-      script.init(); // Assuming you have an init function
+      <h1>Play Tic Tac Toe</h1>
+      <div id="turn">x's Turn</div>
+      <table>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </PrimePanel>
+  </template>
+  
+  <script>
+  import script from '@/assets/script.js'; // Adjust based on your exports
+  
+  export default {
+    mounted() {
+      if (typeof script.init === 'function') {
+        script.init(); // Call init if it exists
+      }
     }
-  }
-};
-</script>
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your CSS here if needed */
+  </style>
+  
