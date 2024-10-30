fa580d0e46a3d5100870d3669da3f89d88d27ba1
Kreibach <kreibach@ksu.edu>
2024-10-30 18:10:23 -0500

    commit

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    presets: [
+      '@nuxt/babel-preset-app' // Default Nuxt preset for applications
+    ],
+    plugins: [
+      // You can add other plugins here if needed
+    ]
+  };
+  
