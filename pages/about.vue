e61ddfa301e5c94dc3c545a765e4d00677454ba5
Kreibach <kreibach@ksu.edu>
2024-10-30 17:52:43 -0500

    commit

--- pages/about.vue
+++ pages/about.vue
@@ -9,13 +9,15 @@
 	  </PrimePanel>
 	
 	  <PrimePanel header="Personal Artwork" class="my-6">
-		<p>I had no idea what to include for the photo section, so here are so projects I made in highschool.</p>
+		<h1>I had no idea what to include for the photo section, so here are so projects I made in highschool.</h1>
 		<p>I made this one as a gift to my first employer, whom I babysat for. <br>July, 2024</p>
 		<img :src="flyImage" alt="Fly Image" class="image_size"/>
 		<p>This was the first time I dealt with acrylic. I did it for a class, but I ended up really enjoying it. 
 			A little unfinished in the details though.
 			<br>May, 2024</p>
 		<img :src="candleImage" alt="Candle Image" class="image_size"/>
+		<p>My favorite watercolor assignment. I just really like it. <br>Dec, 2023</p>
+		<img :src="flowerImage" alt="Flower Image" class="image_size"/>
 		<p>This is just some random artwork I did Covid year in illustration 1. 
 			I only choose this one because it has a clear picture and I found it pretty quickly. It's <i>kinda</i> fun to look at.
 			<br>Dec, 2020</p>
